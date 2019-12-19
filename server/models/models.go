c30bba6d52981335a05ee148cef5a08cfa975d85
David Bee <baeddavid@gmail.com>
2019-12-19 15:04:51 -0600

    add models for books

--- server/models/models.go
+++ server/models/models.go
@@ -1,2 +1,12 @@
 package models
 
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Book struct {
+	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Title string `json:"task, omitempty"`
+	Author string `json:"task,omitempty"`
+	Status string `json:"task,omitempty"`
+}
