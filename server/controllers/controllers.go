bf488069aefd5bc39dc1321e41b64dbc7c2ee279
David Bee <baeddavid@gmail.com>
2019-12-19 23:58:35 -0600

    finish controllers

--- server/controllers/controllers.go
+++ server/controllers/controllers.go
@@ -40,3 +40,132 @@ func init() {
 	collection = client.Database(dbName).Collection(collName)
 }
 
+func GetAllBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	payload := getAllBooks()
+	_ = json.NewEncoder(w).Encode(payload)
+}
+
+func CreateTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content0Type")
+
+	var book models.Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+
+	insertOneBook(book)
+	json.NewEncoder(w).Encode(book)
+}
+
+func BookComplete(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "PUT")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	params := mux.Vars(r)
+	bookComplete(params["id"])
+	_ = json.NewEncoder(w).Encode(params["id"])
+}
+
+func UndoStatus(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "PUT")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	params := mux.Vars(r)
+	undoStatus(params["id"])
+	json.NewEncoder(w).Encode(params["id"])
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	params := mux.Vars(r)
+	deleteOneBook(params["id"])
+	_ = json.NewEncoder(w).Encode(params["id"])
+}
+
+func DeleteAllBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	count := deleteAllBook()
+	json.NewEncoder(w).Encode(count)
+}
+
+func getAllTask() []primitive.M {
+	cur, err := collection.Find(context.Background(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var results []primitive.M
+	for cur.Next(context.Background()) {
+		var result bson.M
+		e := cur.Decode(&result)
+		if e != nil {
+			log.Fatal(e)
+		}
+		results = append(results, result)
+	}
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	_ = cur.Close(context.Background())
+	return results
+}
+
+func insertOneBook(book models.Book) {
+	insertResult, err := collection.InsertOne(context.Background(), book)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted a single record: ", insertResult.InsertedID)
+}
+
+func bookComplete(book string) {
+	fmt.Println(book)
+	id, _ := primitive.ObjectIDFromHex(book)
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"status": true}}
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("modified count: ", result.ModifiedCount)
+}
+
+func deleteOneBook(book string) {
+	fmt.Println(book)
+	id, _ := primitive.ObjectIDFromHex(book)
+	filter := bson.M{"_id": id}
+	d, err := collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Deleted Document", d.DeletedCount)
+}
+
+func deleteAllBook() int64 {
+	d, err := collection.DeleteMany(context.Background(), bson.D{{}}, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Deleted Document", d.DeletedCount)
+	return d.DeletedCount
+}
--- server/models/models.go
+++ server/models/models.go
@@ -8,5 +8,5 @@ type Book struct {
 	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Title string `json:"task, omitempty"`
 	Author string `json:"task,omitempty"`
-	Status string `json:"task,omitempty"`
+	Status bool `json:"task,omitempty"`
 }
